dda3e44445e8f016aefd2547b7da9df47637cadb
Alec von Barnekow <von.barnekow.alec@gmail.com>
2018-10-30 15:56:16 +0100

    Add google auth support

--- src/server/routes.ts
+++ src/server/routes.ts
@@ -1,9 +1,20 @@
 import * as Router from 'koa-router';
+import * as passport from "passport";
 
 const router = new Router();
 
 router.get('/', async (ctx) => {
-  ctx.body = 'Hello World!';
+  await ctx.render('home.handlebars');
 });
+
+router.get('/login/google/callback', async (ctx) => {
+    return passport.authenticate('google', {failureRedirect: '/login'}, (err, user, info, status) => {
+      ctx.redirect('/');
+    })(ctx);
+  }
+);
+
+router.get('/login/google', passport.authenticate('google', {scope: 'email'}));
+
 
 export const routes = router.routes();
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,10 +1,42 @@
 import * as Koa from 'koa';
+import * as views from 'koa-views';
+import * as bodyparser from 'koa-bodyparser';
+import * as session from 'koa-session';
+import * as passport from 'koa-passport';
+import * as GoogleStrategy from 'passport-google-oauth';
 
 import { config } from './config';
 import { logger } from './logging';
 import { routes } from './routes';
 
 const app = new Koa();
+
+passport.use(new GoogleStrategy.OAuth2Strategy({
+    clientID: '',
+    clientSecret: '',
+    callbackURL: '/login/google/callback',
+  },
+  function(accessToken, refreshToken, profile, done) {
+    // TODO
+    /*User.findOrCreate(..., function (err, user) {
+      done(err, user);
+    });*/
+  }
+));
+
+app.use(views( 'src/views/', {
+    map: {
+      html: 'handlebars'
+    }
+  }
+));
+
+app.use(bodyparser());
+
+app.use(session({}, app));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(logger);
 app.use(routes);
