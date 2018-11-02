a32a1b0491840120f100e6cd3f73fd3c7452d119
Alec von Barnekow <von.barnekow.alec@gmail.com>
2018-11-02 22:53:21 +0100

    Refactor URLs

--- src/contants.ts
+++ src/contants.ts
@@ -1,11 +1,12 @@
 export class Constants {
   // URL
   public static HOME_URL = '/';
-  public static LOGIN_URL = '/login';
+
+  public static LOGIN_GOOGLE_URL = '/login/google';
+  public static LOGIN_GOOGLE_CALLBACK_URL = '/login/google/callback';
   public static LOGOUT_URL = '/logout';
+
   public static DASHBOARD_URL = '/dashboard';
-  public static LOGIN_GOOGLE_URL = '/google/login';
-  public static LOGIN_GOOGLE_CALLBACK_URL = '/google/login/callback';
 
   // Mongodb
   public static MONGODB_CONNECT_URL = 'mongodb://localhost/smw';
