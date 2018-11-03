51a08ad5b25eaaa1e1d7263d10fc41df0c56f03d
Alec von Barnekow <von.barnekow.alec@gmail.com>
2018-11-03 17:25:08 +0100

    Add external login controller

---
+++ src/controllers/authentication.ts
@@ -0,0 +1,13 @@
+import * as Koa from 'Koa';
+import * as passport from 'passport';
+import {Constants} from '../contants';
+
+export const login = passport.authenticate('google', {scope: ['email']});
+
+export const loginCallback = passport.authenticate('google',
+  {successRedirect: Constants.DASHBOARD_URL, failureRedirect: Constants.HOME_URL});
+
+export const logout = async (ctx: Koa.Context) => {
+  ctx.logout();
+  ctx.redirect(Constants.HOME_URL);
+};
