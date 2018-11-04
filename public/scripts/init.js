b818942accc0e399210576fab2e082d4d6be632e
Alec von Barnekow <von.barnekow.alec@gmail.com>
2018-11-04 23:18:36 +0100

    Add ui init scripts

--- public/scripts/init.js
+++ public/scripts/init.js
@@ -1,4 +1,31 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, null);
+    var modals = M.Modal.init(document.querySelectorAll('.modal'), null);
+
+    M.Sidenav.init(document.querySelectorAll('.sidenav'), null);
+
+    M.Collapsible.init(document.querySelector('.collapsible.expandable'), {
+        accordion: false
+    });
+
+    var dropdowns = document.querySelectorAll('.dropdown-trigger');
+    M.Dropdown.init(dropdowns, null);
+    // prevent that collapsible opens when user click on the dropdown
+    dropdowns.forEach(function (dropdown) {
+      dropdown.addEventListener('click', function (event) {
+        event.stopPropagation();
+      });
+    });
+
+    var dropdownLinks = document.querySelectorAll('.not-collapse');
+    dropdownLinks.forEach(function (dropdownLink) {
+      dropdownLink.addEventListener('click', function (event) {
+        event.stopPropagation();
+        // open modal with stop propagation
+        modals.forEach(function (modal) {
+          if ('#' + modal.id == dropdownLink.getAttribute('href')) {
+            modal.open();
+          }
+        });
+      });
+    });
 });
