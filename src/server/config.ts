909c72ce8f48764105f276df09edf78da9889c08
Alec von Barnekow <von.barnekow.alec@gmail.com>
2018-11-04 23:14:48 +0100

    Remove config interface

--- src/server/config.ts
+++ src/server/config.ts
@@ -1,8 +1,3 @@
-interface IConfig {
-  port: number;
-  prettyLog: boolean;
-}
-
 export const config = {
   port: process.env.NODE_PORT || 3030,
   prettyLog: process.env.NODE_ENV == 'development',
